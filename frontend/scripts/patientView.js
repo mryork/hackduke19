e5913cd68379419958b1fc14c0ea6b044619a977
adamc77 <1231862.student@gmail.com>
2019-11-02 17:40:18 -0400

    patientView goodness

--- frontend/scripts/patientView.js
+++ frontend/scripts/patientView.js
@@ -2,18 +2,18 @@
 
 export const renderLog = function(log) {
     $("#root .logView").replaceWith(`
-    <div class="logView">
+    <div class="logView column is-four-fifths">
         <div class="logDate">
             <h1>Date: ${log.date}</h1>
         </div>
         <div class="logMood">
             <h1>Mood</h1>
             <div class="logMoodSelector">
-                <img class="mood" id="1" src="frontend/res/mood1.png" alt="Bad Mood">
-                <img class="mood" id="2" src="frontend/res/mood2.png" alt="Not Great Mood">
-                <img class="mood" id="3" src="frontend/res/mood3.png" alt="OK Mood">
-                <img class="mood" id="4" src="frontend/res/mood4.png" alt="Good Mood">
-                <img class="mood" id="5" src="frontend/res/mood5.png" alt="Great Mood">
+                <img class="mood" id="1" src="/imgs/mood1_inactive.png" alt="Bad Mood">
+                <img class="mood" id="2" src="/imgs/mood2_inactive.png" alt="Not Great Mood">
+                <img class="mood" id="3" src="/imgs/mood3_inactive.png" alt="OK Mood">
+                <img class="mood" id="4" src="/imgs/mood4_inactive.png" alt="Good Mood">
+                <img class="mood" id="5" src="/imgs/mood5_inactive.png" alt="Great Mood">
             </div>
         </div>
         <div class="logDesc">
@@ -24,10 +24,12 @@ export const renderLog = function(log) {
     </div>
     `);
 
-    $(`#root .logMoodSelector .mood #${log.object.mood}`).addClass("active");
+    $(`#root .logMoodSelector #${log.object.mood}`).attr("src", `/imgs/mood${log.object.mood}_active.png`);
     $("#root .logMoodSelector .mood").on("click", function(evt) {
-        $("#root .logMoodSelector .mood").removeClass("active");
-        $(`#root .logMoodSelector .mood #${evt.target.id}`).addClass("active");
+        for (var i = 0; i < 5; i++) {
+            $(`#root .logMoodSelector #${log.object.mood}`).attr("src", `/imgs/mood${log.object.mood}_inactive.png`);
+        }
+        $(`#root .logMoodSelector #${evt.target.id}`).attr("src", `/imgs/mood${evt.target.id}_active.png`);
         log.object.mood = evt.target.id;
     });
 
@@ -39,12 +41,23 @@ export const renderLog = function(log) {
 }
 
 export const loadView = function() {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
     // render log dates listview
     getLogs().then(y => {
+        let todaysDate = new Date();
+        let today = y.find(x => {
+            let date = new Date(x.date);
+            return (date.getDate() == todaysDate.getDate() &&
+                    date.getMonth() == todaysDate.getMonth() &&
+                    date.getFullYear() == todaysDate.getFullYear()) ? x : false;
+        });
+        if (!today) { saveLog(); }
         y.forEach(x => {
+            let date = new Date(x.date);
             $("#root .datePanel").append(`
             <div id="${x.id}" class="dateObj">
-                <h1>${x.date}</h1>
+                <h1>${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}</h1>
             </div>
             `);
             $(`#root .datePanel #${x.id}`).on("click", function() {
@@ -53,6 +66,10 @@ export const loadView = function() {
                 renderLog(x);
             });
         });
+        if (today) {
+            $(`#root .datePanel #${today.id}`).addClass("activeDate");
+            renderLog(today);
+        }
     });
 }
 

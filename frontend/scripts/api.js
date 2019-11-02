6b7bc1a2e2d8f5701e5658b118204aa749da0f4f
adamc77 <1231862.student@gmail.com>
2019-11-02 19:13:59 -0400

    patientView nearing copmpletion

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -2,17 +2,17 @@ function getLogs() {
     return new Promise((res, rej) => {
         setTimeout(() => {
             res([
-                {
-                    id: "3udiuw2",
-                    date: Date.now() - 1000,
-                    object: {
-                        mood: 5,
-                        message: "Wow!"
-                    }
-                },
+                // {
+                //     id: "3udiuw2",
+                //     date: Date.now() - 1000,
+                //     object: {
+                //         mood: 5,
+                //         message: "Wow!"
+                //     }
+                // },
                 {
                     id: "3udiuq3221nw2",
-                    date: Date.now() - 1500,
+                    date: Date.now() - 150000000,
                     object: {
                         mood: 3,
                         message: "coool"
@@ -27,4 +27,10 @@ function saveLog(log) {
     return new Promise((res, rej) => {
         res("Saved!");
     })
+}
+
+function emptyLog() {
+    return new Promise((res, rej) => {
+       res("Created!"); 
+    });
 }

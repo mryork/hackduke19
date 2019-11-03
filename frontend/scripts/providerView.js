c8e704c68c927d9196f0f7ed4180800ee17638c8
adamc77 <1231862.student@gmail.com>
2019-11-03 12:25:35 -0500

    log[i] uh

--- frontend/scripts/providerView.js
+++ frontend/scripts/providerView.js
@@ -60,7 +60,7 @@ async function getSentiments(logs) {
         var sentimentData = [];
 
         for (var i = 0; i < logs.length; i++) {
-            analyzeSentiment(log.object.message).then(sent => {
+            analyzeSentiment(logs[i].object.message).then(sent => {
                 sentimentData.push(Math.floor((Number(sent.sentiment)+2)*5/3));
             });
         }

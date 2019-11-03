103c302f664b3c049d35617e93ab0d65a333a11c
adamc77 <1231862.student@gmail.com>
2019-11-03 11:16:14 -0500

    graphs coming together

--- frontend/scripts/providerView.js
+++ frontend/scripts/providerView.js
@@ -76,16 +76,16 @@ export const loadStats = function(id) {
                 labels: dates,
                 datasets: [{
                     label: 'Patient Mood',
-                    backgroundColor: 'rgba(10, 240, 240, .5)',
-                    borderColor: 'rgba(10, 240, 240, .5)',
+                    backgroundColor: 'rgba(242, 48, 51, .5)',
+                    borderColor: 'rgba(242, 48, 51, .5)',
                     data: moodData,
                     fill: false
                 }, {
                     label: 'Patient Sentiment',
-                    backgroundColor: 'rgba(240, 240, 10, .5)',
-                    borderColor: 'rgba(240, 240, 10, .5)',
-                    data: moodData,
-                    data: sentimentData
+                    backgroundColor: 'rgba(48, 54, 242, .5)',
+                    borderColor: 'rgba(48, 54, 242, .5)',
+                    data: sentimentData,
+                    fill: false
                 }]
             },
             options: {
